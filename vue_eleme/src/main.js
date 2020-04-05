cd3645e38bf293b6f666936eb92a729e7933143b
Liu-xc <1392166232@qq.com>
2020-04-05 23:54:05 +0800

    nothing

--- vue_eleme/src/main.js
+++ vue_eleme/src/main.js
@@ -8,7 +8,7 @@ import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
-import 'swiper/dist/css/swiper.css'
+require('swiper/dist/css/swiper.css')
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
