b8678491d3de9709710df149f5923362f2c7c9e1
Liu-xc <1392166232@qq.com>
2020-04-05 23:42:16 +0800

    swiper

--- vue_eleme/src/pages/home/Home.vue
+++ vue_eleme/src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/Swiper.vue'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ vue_eleme/src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+ <swiper :options="swiperOption">
+     <!-- slides -->
+     <swiper-slide v-for="item of swiperList" :key="item.id">
+       <img class="swiper-img"  :src="item.imgUrl" />
+     </swiper-slide>
+     <!-- Optional controls -->
+     <div class="swiper-pagination"  slot="pagination"></div>
+   </swiper>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: 'true'
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/5d7c1bffddc1a75cbfb59a389e68583f.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/c95ee8fe1700026cad03bc4852112592.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+      background red !important
+  .wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 31.25%
+    .swiper-img
+      width 100%
+</style>
