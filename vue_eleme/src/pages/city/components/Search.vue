279563dcb54336e5b9a1bacf0413c9d35d5102bf
Liu-xc <1392166232@qq.com>
2020-04-06 21:26:10 +0800

    add search to city

---
+++ vue_eleme/src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height .72rem
+    padding 0 .1rem
+    background-color $bg-color
+    .search-input
+      height .62rem
+      line-height .62rem
+      width 100%
+      text-align center
+      border-radius .06rem
+      color #666
+      padding 0 .1rem
+      box-sizing border-box
+</style>
