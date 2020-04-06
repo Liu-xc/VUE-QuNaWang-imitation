728df82621ef54ebb60489c93e1d08fa3e8ef73e
Liu-xc <1392166232@qq.com>
2020-04-06 15:49:04 +0800

    index finish

--- vue_eleme/src/pages/home/components/weekend.vue
+++ vue_eleme/src/pages/home/components/weekend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">周末去哪</div>
     <ul>
-      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
         <div class="item-img-wrapper">
           <img class="item-img" :src='item.imgUrl' />
         </div>
@@ -18,25 +18,8 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/a2/78e4dbcfd45a6d.jpg_r_640x214_7a62b06a.jpg',
-        title: '药王谷',
-        desc: '药王谷所在区域盛产中药材，山林遍生百年药树，山上居民一直有供奉药王菩萨的习俗，药王谷因此得名。'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/65/7322301be0a92c.jpg_r_640x214_a3485c55.jpg',
-        title: '药王谷',
-        desc: '药王谷所在区域盛产中药材，山林遍生百年药树，山上居民一直有供奉药王菩萨的习俗，药王谷因此得名。'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1502/e0/e055cb2c79da8f33.water.jpg_200x200_93193a93.jpg',
-        title: '药王谷',
-        desc: '药王谷所在区域盛产中药材，山林遍生百年药树，山上居民一直有供奉药王菩萨的习俗，药王谷因此得名。'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
@@ -47,11 +30,10 @@ export default {
     line-height .8rem
     background-color #EAEAEA
     text-indent .2rem
-    margin-top .2rem
   .item-img-wrapper
     height 0
     overflow hidden
-    padding-bottom 33.9%
+    padding-bottom 37.09%
     .item-img
       width 100%
       height auto
