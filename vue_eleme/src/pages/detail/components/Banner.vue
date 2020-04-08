786c587a827dfdfaf19e795832c319de257d59b6
Liu-xc <1392166232@qq.com>
2020-04-08 19:06:02 +0800

    animation finish

--- vue_eleme/src/pages/detail/components/Banner.vue
+++ vue_eleme/src/pages/detail/components/Banner.vue
@@ -12,16 +12,19 @@
           </div>
       </div>
     </div>
-    <common-gallary
-      :imgs="bannerImgs"
-      v-show="showGallary"
-      @close="handleGallaryClose"
-      ></common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs="bannerImgs"
+        v-show="showGallary"
+        @close="handleGallaryClose"
+        ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation.vue'
 export default {
   name: 'DetailBanner',
   props: {
@@ -35,7 +38,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   methods: {
     handleBannerClick () {
