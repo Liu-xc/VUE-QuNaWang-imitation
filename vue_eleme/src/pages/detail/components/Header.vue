7980204355e3094092f523e666278256e7d5f2ed
Liu-xc <1392166232@qq.com>
2020-04-08 13:57:17 +0800

    fix problem

--- vue_eleme/src/pages/detail/components/Header.vue
+++ vue_eleme/src/pages/detail/components/Header.vue
@@ -46,6 +46,9 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
